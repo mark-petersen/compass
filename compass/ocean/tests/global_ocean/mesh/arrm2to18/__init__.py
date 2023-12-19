db707f092fe07a09d77ed170e968e7fb5f1fef73
Mark Petersen <mpetersen@lanl.gov>
2023-12-19 15:54:55 -0700

    Revert "try zonal lat3to18"
    This reverts commit 87449c7378eb8aeabc1ba0faefe306a5c84f63d9.

--- compass/ocean/tests/global_ocean/mesh/arrm2to18/__init__.py
+++ compass/ocean/tests/global_ocean/mesh/arrm2to18/__init__.py
@@ -83,11 +83,11 @@ class ARRM2to18BaseMesh(QuasiUniformSphericalMeshStep):
 
         # Expand from 1D to 2D. Pick one of these:
 
-        QU1D = 18.0*np.ones(lat.size)
-        _, cellWidthLowRes = np.meshgrid(lon, QU1D)
+        #QU1D = 18.0*np.ones(lat.size)
+        #_, cellWidthLowRes = np.meshgrid(lon, QU1D)
 
-        #RRS1D = mdt.RRS_CellWidthVsLat(lat, 18.0, 6.0)
-        #_, cellWidthLowRes = np.meshgrid(lon, RRS1D)
+        RRS1D = mdt.RRS_CellWidthVsLat(lat, 18.0, 6.0)
+        _, cellWidthLowRes = np.meshgrid(lon, RRS1D)
 
         #RRS1D = mdt.RRS_CellWidthVsLat(lat, 25.0, 10.0)
         #_, cellWidthLowRes = np.meshgrid(lon, RRS1D)
@@ -116,10 +116,7 @@ class ARRM2to18BaseMesh(QuasiUniformSphericalMeshStep):
                                   (transitionWidth / 2.)))
         #del soon: midToHighResMask = np.maximum(midToHighResMask, mask)
 
-        #tmp cellWidthHighRes = highRes*midToHighResMask + midRes*(1-midToHighResMask)
-#tmp make high res region constant
-        QU1D = highRes*np.ones(lat.size)
-        _, cellWidthHighRes = np.meshgrid(lon, QU1D)
+        cellWidthHighRes = highRes*midToHighResMask + midRes*(1-midToHighResMask)
 
 ########################################################################
 #
@@ -135,10 +132,8 @@ class ARRM2to18BaseMesh(QuasiUniformSphericalMeshStep):
         # Expand from 1D to 2D
         _, midToHighResMask = np.meshgrid(lon, midToHighResTanh)
 # start the high res mask as zeros, and add ones for the high res region
-        #tmp latMid = 46.0
-        #tmp latTransitionWidth = 5.0
-        latMid = 38.0
-        latTransitionWidth = 10.0
+        latMid = 46.0
+        latTransitionWidth = 5.0
         # create 1D array as a function of latitude
         highResTanh = 0.5 + 0.5*np.tanh( (lat - latMid) / latTransitionWidth )
         _, highResMask = np.meshgrid(lon, highResTanh)
@@ -155,7 +150,7 @@ class ARRM2to18BaseMesh(QuasiUniformSphericalMeshStep):
                                                       max_length=0.25)
         mask = 0.5 * (1 + np.tanh((transitionOffset - signedDistance) /
                                   (transitionWidth / 2.)))
-        #tmp highResMask = np.maximum(highResMask, mask)
+        highResMask = np.maximum(highResMask, mask)
 
         fileName = 'coastline_CUSP'
         transitionWidth = transitionWidthGlobal
@@ -166,7 +161,7 @@ class ARRM2to18BaseMesh(QuasiUniformSphericalMeshStep):
                                                       max_length=0.25)
         mask = 0.5 * (1 + np.tanh((transitionOffset - signedDistance) /
                                   (transitionWidth / 2.)))
-        #tmp highResMask = np.maximum(highResMask, mask)
+        highResMask = np.maximum(highResMask, mask)
 
         fileName = 'region_Gulf_of_Mexico'
         transitionOffset = 800.0 * km
@@ -184,7 +179,7 @@ class ARRM2to18BaseMesh(QuasiUniformSphericalMeshStep):
                                                       max_length=0.25)
         landMask = 0.5 * (1 + np.sign(-signedDistance))
         mask = maskSharp * landMask + maskSmooth * (1 - landMask)
-        #tmp highResMask = np.maximum(highResMask, mask)
+        highResMask = np.maximum(highResMask, mask)
 
 ########################################################################
 #
@@ -204,7 +199,7 @@ class ARRM2to18BaseMesh(QuasiUniformSphericalMeshStep):
                                                       max_length=0.25)
         mask = 0.5 * (1 + np.tanh((transitionOffset - signedDistance) /
                                   (transitionWidth / 2.)))
-        #tmp cellWidth = BalticRes * mask + cellWidth * (1 - mask)
+        cellWidth = BalticRes * mask + cellWidth * (1 - mask)
 
         fileName = 'region_Mediterranean_Sea'
         transitionWidth = 50*km
@@ -216,7 +211,7 @@ class ARRM2to18BaseMesh(QuasiUniformSphericalMeshStep):
                                                       max_length=0.25)
         mask = 0.5 * (1 + np.tanh((transitionOffset - signedDistance) /
                                   (transitionWidth / 2.)))
-        #tmp cellWidth = MediterraneanRes * mask + cellWidth * (1 - mask)
+        cellWidth = MediterraneanRes * mask + cellWidth * (1 - mask)
 
         #_plot_cartopy(plotFrame, fileName + ' mask', mask, 'Blues')
         #_plot_cartopy(plotFrame + 1, 'cellWidth ', cellWidth, '3Wbgy5')
